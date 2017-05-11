82f71d13a7d06e4cc829a690ea2324837d96f808
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-05-11 11:40:59 +0200

    add sass and font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import './styles/styles.css';
+
+import App from './components/App';
+
 render(
-  <h1>Hello React</h1>,
+  <App />,
   document.getElementById('root')
 );
