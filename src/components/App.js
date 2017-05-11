88afb8e0083db168a88a7bbe0252bceba7ce72d8
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-05-11 12:20:05 +0200

    add bootstrap and choosing player

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playerMark: null,
+      computerMark: null
+    }
+  }
   render() {
     return (
-      <div>
-        <div className="title">
-          <h1>Kółko i <br/>Krzyżyk</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-3 title">
+            <h1>Kółko i <br/>Krzyżyk</h1>
+          </div>
+          {
+            this.state.playerMark == null 
+            ? <div className="col-sm-9 choosePlayer text-center">
+              <h2>Wybierz Gracza</h2>
+              <button 
+                className="marks btn"
+                onClick={() => this.setState({ playerMark: 'X', computerMark: 'O'})}
+              >
+              X
+              </button>
+              <button 
+                className="marks btn"
+                onClick={() => this.setState({ playerMark: 'O', computerMark: 'X'})}
+              >
+              O
+              </button>
+            </div>
+            : <div></div>
+          }
+          
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
 import App from './components/App';
